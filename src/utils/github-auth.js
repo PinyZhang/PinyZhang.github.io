1008d6a2db0096f52170f945673a340ea388000c
zhangzhonghao <zhangzhonghao@kuaishou.com>
2023-04-21 17:55:10 +0800

    build: update

--- src/utils/github-auth.js
+++ src/utils/github-auth.js
@@ -29,7 +29,7 @@ export async function githubCallback() {
         Accept: 'application/json'
     };
 
-    const response = await fetch(`https://api.github.com/login/oauth/access_token?${new URLSearchParams(params)}`, {
+    const response = await fetch(`https://github.com/login/oauth/access_token?${new URLSearchParams(params)}`, {
         headers,
         method: 'POST',
     });
